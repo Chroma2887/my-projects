d7c3d783486c112e292dac92b3b444647a9ec442
Chroma2887 <110918661+Chroma2887@users.noreply.github.com>
2026-07-05 23:53:14 +0800

    js-series-4-6-kenwheeler/slick 套件

---
+++ javascript4-6/README.md
@@ -0,0 +1,6 @@
+# JavaScript 系列四：練習六
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/qzmmcdjs-the-styleful/pen/JoEMoZJ](https://codepen.io/qzmmcdjs-the-styleful/pen/JoEMoZJ).
+
---
+++ javascript4-6/dist/script.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  centerMode: true,
+  centerPadding: '0px',
+  focusOnSelect: true,
+  speed: 200
+});
